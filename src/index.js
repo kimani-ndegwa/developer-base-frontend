f174e1e0078c9efed703143fc9004316d57612d9
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-11-08 20:49:03 +0300

    Configure react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Home} from './Home';
+import { Home } from './Home';
+import { DevelopersPage } from './Developers'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home/>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/developers' component={DevelopersPage} />
+        </Switch>
+    </Router>
+
+
+    , document.getElementById('root'));
 registerServiceWorker();
